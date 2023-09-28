1bdc5601f66302a57881ad6b273ca809e66d9c73
Mark Raden <mraden@csu.fullerton.edu>
2023-09-28 16:34:15 -0700

    first commit

---
+++ week_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week_7
+//
+//  Created by Mark D. Raden on 9/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
